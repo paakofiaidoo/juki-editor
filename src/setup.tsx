aa3a0faa510b09e614d3a776e6808912ee9f3b6d
paakofiaidoo <paakofiaidoo17@gmail.com>
2025-05-04 03:29:14 +0000

    fix!:

--- src/setup.tsx
+++ src/setup.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { useRouter } from 'next/navigation';
-import { useProjectStore, Project } from '../state/projectStore';
+import { useProjectStore, Project } from './state/projectStore';
 
 interface FormData extends Project {}
 
